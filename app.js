5758c8905e5a618de2368d9758a2559f59bc9669
Sabbadini10 <matiassabbadini24@gmail.com>
2022-06-24 08:21:49 -0300

    Cree carpetas

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath))
+
+
+app.listen(3030, () => {
+    console.log('Server is running on port 3030');
+    });
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve('./view/home.html'));
+    });
